a695b02272aac7402cfbc1654286c97a52bdcec8
Alex Walrond <alexbentonlee@gmail.com>
2024-06-23 23:23:48 -0500

    Module 6 completed baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -34,8 +34,8 @@ const tripsFindByCode = async(req, res) => {
     const q = await Model
         .find({'code' : req.params.tripCode }) // Return single record
         .exec();
-
-    // uncomment the following line to show results of querey
+    
+        // uncomment the following line to show results of querey
     // on the console
     // console.log(q);
     
@@ -52,7 +52,85 @@ const tripsFindByCode = async(req, res) => {
 
 };
 
+// POST: /trips - Adds a new Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsAddTrip = async(req, res) => {
+    const newTrip = new Tri({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    });
+
+    const q = await newTrip.save();
+
+        if(!q)
+        { // Database returned no data
+            return res
+                .status(400)
+                .json(err)
+        } else { // Return new trip
+            return res
+                .status(201)
+                .json(q);
+        }
+
+        // Uncomment the following line to show reults of operation
+        // on the console
+        // console.log(q);
+};
+
+// PUT: /trips/:tripCode - Updates a Trip
+// Regardless of outcome, response must include HTML status code
+
+// PUT: /trips/:tripCode - Adds a new Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsUpdateTrip = async (req, res) => {
+    // Uncomment for debugging
+console.log(req.params);
+console.log(req.body);
+    try {
+        const q = await Model.findOneAndUpdate(
+            { 'code': req.params.tripCode },
+            {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            }
+        )
+        .exec();
+        if (!q) {
+            // Database returned no data
+            return res
+                .status(400)
+                .json(err)
+        } else {
+            // Return resulting updated trip
+            return res
+                .status(201)
+                .json(q)
+        }
+    }
+    
+    // Uncomment the following line to show results of operation
+    // on the console
+    // console.log(q);
+};
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
